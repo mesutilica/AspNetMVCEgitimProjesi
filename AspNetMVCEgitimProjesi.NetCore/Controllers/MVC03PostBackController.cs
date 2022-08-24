dd47c17580bd6dc15236cc87f4a9215f16fdb1b2
mesutilica <mesut.ilica@gmail.com>
2022-08-24 22:06:36 +0300

    24-08-2022 Post back bitti

---
+++ AspNetMVCEgitimProjesi.NetCore/Controllers/MVC03PostBackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetCore.Controllers
+{
+    public class MVC03PostBackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMVCEgitimProjesi.NetFramework/Controllers/MVC03PostBackController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetFramework.Controllers
+{
+    public class MVC03PostBackController : Controller
+    {
+        // GET: MVC03PostBack
+        [HttpGet] // Eğer bir action un üstüne hiçbir metot attribute ü yazılmamışsa varsayılan metot httpget dir
+        public ActionResult Index(string kelime)
+        {
+            ViewBag.Mesaj = "Get metoduyla gönderilen değer : " + kelime;
+            return View();
+        }
+        [HttpPost] // Sayfada post metodu çalıştırılırsa aşağıdaki action çalışır
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

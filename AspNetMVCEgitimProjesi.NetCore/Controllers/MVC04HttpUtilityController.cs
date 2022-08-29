1319553f5778759751280a04907b4dc92d2a6ec0
mesutilica <mesut.ilica@gmail.com>
2022-08-29 23:07:03 +0300

    29-08-2022 partial bitti

---
+++ AspNetMVCEgitimProjesi.NetCore/Controllers/MVC04HttpUtilityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetCore.Controllers
+{
+    public class MVC04HttpUtilityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMVCEgitimProjesi.NetCore/Controllers/MVC06SectionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetCore.Controllers
+{
+    public class MVC06SectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

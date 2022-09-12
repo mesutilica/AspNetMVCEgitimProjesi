ae7a26e4e7905954f9ffae22baac6aaa4901fd2f
mesutilica <mesut.ilica@gmail.com>
2022-09-12 23:01:19 +0300

    12-09-2022 Users bitti

--- AspNetMVCEgitimProjesi.NetCore/Areas/Admin/Models/User.cs
+++ AspNetMVCEgitimProjesi.NetCore/Areas/Admin/Models/User.cs
@@ -1,11 +1,17 @@
-﻿namespace AspNetMVCEgitimProjesi.NetCore.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMVCEgitimProjesi.NetCore.Areas.Admin.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Display(Name = "Adı")]
         public string Name { get; set; }
+        [Display(Name = "Soyadı")]
         public string Surname { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
         public string Username { get; set; }
+        [Display(Name = "Şifre")]
         public string Password { get; set; }
         public string Email { get; set; }
     }

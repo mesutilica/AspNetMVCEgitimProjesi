a5d0b4f1920a9c86a8005724c007a13f9f3f4053
mesutilica <mesut.ilica@gmail.com>
2022-09-30 22:05:37 +0300

    30-09-2022 mvc dosya yükleme

---
+++ AspNetMVCEgitimProjesi.NetCore/Controllers/MVC15AppSettingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetCore.Controllers
+{
+    public class MVC15AppSettingController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MVC15AppSettingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            TempData["Email"] = _configuration["Email"]; // TempData ile appsettings deki Email bilgisini okuyup view a gönderdik
+            TempData["MailSunucu"] = _configuration["MailSunucu"];
+            TempData["UserName"] = _configuration["MailKullanici:UserName"];
+            TempData["Password"] = _configuration.GetSection("MailKullanici:Password").Value;
+            return View();
+        }
+    }
+}

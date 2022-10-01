3d08d32308652e43f9019cc8cb04541c9c362b80
Mesut ILICA <mesut.ilica@gmail.com>
2022-10-01 17:02:25 +0300

    1-10-2022 View components

---
+++ AspNetMVCEgitimProjesi.NetCore/ViewComponents/Kategoriler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetCore.ViewComponents
+{
+    public class Kategoriler : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

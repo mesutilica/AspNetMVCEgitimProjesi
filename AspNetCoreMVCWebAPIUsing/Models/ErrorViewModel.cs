2f408c5ac0e650697a4186a57488790c3bfa58f6
Mesut ILICA <mesutilica@outlook.com>
2023-11-26 22:47:23 +0300

    Web api kullanım projesi eklendi

---
+++ AspNetCoreMVCWebAPIUsing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreMVCWebAPIUsing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

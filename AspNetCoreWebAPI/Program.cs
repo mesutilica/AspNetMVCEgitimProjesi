ef9b58724674daa2861a3dfabf12cefedbbd5a40
Mesut ILICA <mesut.ilica@gmail.com>
2024-03-09 07:36:26 +0300

    Login + jwt ayarları eklendi

--- AspNetCoreWebAPI/Program.cs
+++ AspNetCoreWebAPI/Program.cs
@@ -1,5 +1,8 @@
 using AspNetCoreWebAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace AspNetCoreWebAPI
@@ -19,12 +22,30 @@ namespace AspNetCoreWebAPI
 
             builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // json dan çekmek için
 
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    //Validasyon yapmak istediðimiz alanlar
+                    ValidateAudience = true, // Kitleyi Doðrula
+                    ValidateIssuer = true, // Tokený vereni doðrula
+                    ValidateLifetime = true, // Token yaþam süresini doðrula
+                    ValidateIssuerSigningKey = true, // Tokený verenin Ýmzalama anahtarýný Doðrula
+                    ValidIssuer = builder.Configuration["Token:Issuer"], // Tokený veren saðlayýcý
+                    ValidAudience = builder.Configuration["Token:Audience"], // Tokený kullanacak kullanýcý
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])), // Tokený Ýmzalama Anahtarý
+                    ClockSkew = TimeSpan.Zero // saat farký olmasýn
+                };
+            });
+
             builder.Services.AddCors(options =>
             {
                 // this defines a CORS policy called "default"
                 options.AddPolicy("default", policy =>
                 {
-                    policy.WithOrigins("https://localhost:7262")
+                    policy
+                        .AllowAnyOrigin() // tümüne izin ver
+                        // .WithOrigins("https://localhost:7262") // sadece bu domainlere izin ver
                         .AllowAnyHeader()
                         .AllowAnyMethod();
                 });

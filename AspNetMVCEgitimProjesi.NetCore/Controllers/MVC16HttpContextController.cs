f98ccdc062dbd5ba80eca6a84237f75d7cd5a642
Mesut ILICA <mesut.ilica@gmail.com>
2025-03-17 23:08:04 +0300

    MVC16HttpContextController konuları güncellendi

--- AspNetMVCEgitimProjesi.NetCore/Controllers/MVC16HttpContextController.cs
+++ AspNetMVCEgitimProjesi.NetCore/Controllers/MVC16HttpContextController.cs
@@ -7,11 +7,10 @@ namespace AspNetMVCEgitimProjesi.NetCore.Controllers
     {
         public IActionResult Index()
         {
-            var mesaj = "RouteData controller : " + HttpContext.GetRouteValue("Controller");
-            mesaj += "<hr/>Action : " + HttpContext.GetRouteValue("Action");
-            mesaj += "<hr/>Id : " + HttpContext.GetRouteValue("id");
+            var mesaj = "RouteData controller : " + RouteData.Values["controller"];
+            mesaj += "<hr/>Action : " + RouteData.Values["action"];
+            mesaj += "<hr/>Id : " + RouteData.Values["id"];
             mesaj += "<hr/>QueryString Kelime : " + HttpContext.Request.Query["kelime"];
-            mesaj += "<hr/> Tam Url-GetEncodedUrl : " + UriHelper.GetEncodedUrl(Request);
             mesaj += "<hr/> Tam Url-GetDisplayUrl : " + UriHelper.GetDisplayUrl(Request);
             mesaj += "<hr/> Tam Url-GetEncodedPathAndQuery : " + UriHelper.GetEncodedPathAndQuery(Request);
             TempData["mesaj"] = mesaj;

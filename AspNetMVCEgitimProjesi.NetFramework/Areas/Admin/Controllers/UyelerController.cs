51f766d68b33f0f3f75ccb83be2626120328b58e
Mesut ILICA <mesut.ilica@gmail.com>
2025-03-29 18:34:46 +0300

    Area konusu güncellendi

---
+++ AspNetMVCEgitimProjesi.NetFramework/Areas/Admin/Controllers/UyelerController.cs
@@ -0,0 +1,120 @@
+﻿using AspNetMVCEgitimProjesi.NetFramework.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace AspNetMVCEgitimProjesi.NetFramework.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class UyelerController : Controller
+    {
+        private UyeContext db = new UyeContext();
+
+        // GET: MVC06CRUD
+        public ActionResult Index()
+        {
+            return View(db.Uyeler.ToList());
+        }
+
+        // GET: MVC06CRUD/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyeler.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // GET: MVC06CRUD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MVC06CRUD/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Uyeler.Add(uye);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uye);
+        }
+
+        // GET: MVC06CRUD/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyeler.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // POST: MVC06CRUD/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uye).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uye);
+        }
+
+        // GET: MVC06CRUD/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyeler.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // POST: MVC06CRUD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Uye uye = db.Uyeler.Find(id);
+            db.Uyeler.Remove(uye);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
